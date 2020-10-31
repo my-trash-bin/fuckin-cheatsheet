c1c50ce88af8c9be2ddd6c65224bcee81b4c68b5
Hyeongwoo Park <hyeopark@c8r6s10.42seoul.kr>
2020-10-31 23:45:27 +0900

    rush02

--- RUSH02/ex00/split.c
+++ RUSH02/ex00/split.c
@@ -6,7 +6,7 @@
 /*   By: hyeopark <hyeopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 22:25:06 by hyeopark          #+#    #+#             */
-/*   Updated: 2020/10/31 22:27:15 by hyeopark         ###   ########.fr       */
+/*   Updated: 2020/10/31 23:43:19 by hyeopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ char	**split(char *str)
 
 	idx = 0;
 	offset = 0;
-	while(idx < count_words(str))
+	array = (char**)malloc(sizeof(char*) * (count_words(str) + 1));
+	while (idx < count_words(str))
 	{
 		word = find_word(str, &length, &offset);
 		array[idx] = (char*)malloc(sizeof(char) * (length + 1));
-		array[length] = 0;
-		
+		strncopy(array[idx], word, length);
 		idx++;
 	}
-
+	array[idx] = NULL;
+	return (array);
 }
